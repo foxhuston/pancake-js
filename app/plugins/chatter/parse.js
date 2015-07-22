fbaaf4051f11d770ec4d75bbdd468cc10a494271
Geoff Huston <geoff.huston@gmail.com>
2015-07-22 18:50:51 -0400

    Working on new parser

--- app/plugins/chatter/parse.js
+++ app/plugins/chatter/parse.js
@@ -1,3 +1,80 @@
-function parse(str) {
 
+
+function tok(str) {
+    var toks = [];
+    var currentTok = '';
+    var matchingLink = false;
+    var punctMatch = /[.?!, ]/;
+
+    var tpush = function() {
+        if(currentTok !== '') {
+            toks.push(currentTok);
+        }
+
+        currentTok = '';
+    };
+
+    while(str !== '') {
+        var currentChar = str[0];
+        
+        if(matchingLink) {
+            var m = str.match(/(^[!, ])|(^\. )|(^\.$)/);
+            if(m) { //no '.'
+                matchingLink = false;
+                tpush();
+                if(m[0][0] !== ' ') {
+                    toks.push(m[0][0]);
+                }
+            } else {
+                currentTok += currentChar;
+            }
+        } else if(str.match(/^http/)) {
+            currentTok += currentChar;
+            matchingLink = true;
+        } else if(currentChar === ' ') {
+            tpush();
+        } else if (currentChar.match(punctMatch)) {
+            tpush();
+            toks.push(currentChar);
+        } else {
+            currentTok += currentChar;
+        }
+
+        str = str.slice(1);
+    };
+    
+    tpush();
+    return toks;
 }
+
+function chunk(toks) {
+    var currentSentence = [];
+    var sentences = [];
+
+    var stopMatch = /(\.\.\.)|[.!?]/
+
+    while(toks.length > 0) {
+        var currentTok = toks[0];
+        currentSentence.push(currentTok);
+
+        if(currentTok.match(stopMatch)) {
+            sentences.push(currentSentence);
+        }
+
+        toks = toks.slice(1);
+    }
+
+    return sentences;
+}
+
+function parse(str) {
+    return tok(str);
+}
+
+var test = 'Hello. This is a test? With a link, this time https://docs.unrealengine.com/latest/INT/Engine/UMG/UserGuide/Fonts/index.html you guys.';
+var test2 = 'Hello. This is a test? With a link, this time https://docs.unrealengine.com/latest/INT/Engine/UMG/UserGuide/Fonts/index.html.';
+var test3 = 'Hello. This is a test? With a link, this time https://docs.unrealengine.com/latest/INT/Engine/UMG/UserGuide/Fonts/';
+var test4 = 'Hello. This is a test? With a link, this time https://docs.unrealengine.com/latest/INT/Engine/UMG/UserGuide/Fonts/.';
+var test5 = 'Hello... Yeah... Yeah.';
+
+console.log(parse(test4));
